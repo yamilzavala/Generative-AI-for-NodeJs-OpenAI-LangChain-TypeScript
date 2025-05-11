e9b4cc22a35a1774c8150f7aa71d180fc2f1b3d9
yamilzavala <zavalayamil9@gmail.com>
2025-05-11 08:50:24 -0300

    chroma vector database added

---
+++ 4.vector-database/src/chroma/Client.ts
@@ -0,0 +1,32 @@
+import { ChromaClient, OpenAIEmbeddingFunction } from 'chromadb'
+
+const client = new ChromaClient({
+    path: 'http://localhost:8000'
+})
+
+const embeddingFunction = new OpenAIEmbeddingFunction({
+    openai_api_key: process.env.OPENAI_API_KEY!,
+    openai_model: 'text-embedding-3-small'
+})
+
+async function main(){
+    const response = await client.createCollection({
+        name: 'data-test2'
+    })
+    console.log(response)
+}
+
+async function addData() {
+    const collection = await client.getCollection({
+        name: 'data-test2',
+        embeddingFunction: embeddingFunction
+    });
+    const result = await collection.add({
+        ids: ['id1'],
+        documents:['Here is my entry'],
+    })
+    console.log(result)
+}
+
+//main
+//addData()
